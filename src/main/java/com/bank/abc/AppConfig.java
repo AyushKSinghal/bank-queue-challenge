c5a078156f6271736167b04dd8cc7e1b3c0a1da2
Ayush Singhal (asinghal) <asinghal@expedia.com>
2017-09-03 00:47:18 +0530

    added more cod

--- src/main/java/com/bank/abc/AppConfig.java
+++ src/main/java/com/bank/abc/AppConfig.java
@@ -1,8 +1,16 @@
 package com.bank.abc;
 
+import com.bank.abc.domain.dataaccess.BankRepository;
+import com.bank.abc.domain.service.BankService;
+import com.bank.abc.domain.service.CounterService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean CounterService getCounterService(BankRepository bankRepository) {
+        return new CounterService(bankRepository);
+    }
 
 }
