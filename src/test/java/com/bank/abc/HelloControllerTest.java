941480b4355bf9bd6a6cfaebd8d21166b0678f58
Ayush Singhal (asinghal) <asinghal@expedia.com>
2017-09-02 21:45:43 +0530

    added project setup fules

---
+++ src/test/java/com/bank/abc/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.bank.abc;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class HelloControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testHello() throws Exception {
+
+    }
+
+}
